894add91db8748b383a713302726469d11c509db
Matthias Kretschmann <m@kretschmann.io>
2022-05-10 00:34:22 +0100

    more web3 calls reduction

--- src/@context/Web3.tsx
+++ src/@context/Web3.tsx
@@ -13,7 +13,7 @@ import { infuraProjectId as infuraId } from '../../app.config'
 import WalletConnectProvider from '@walletconnect/web3-provider'
 import { LoggerInstance } from '@oceanprotocol/lib'
 import { isBrowser } from '@utils/index'
-import { getEnsProfile } from '@utils/ens'
+import { getEnsAvatar, getEnsName, getEnsProfile } from '@utils/ens'
 import { getOceanBalance } from '@utils/ocean'
 import useNetworkMetadata, {
   getNetworkDataById,
@@ -169,7 +169,8 @@ function Web3Provider({ children }: { children: ReactNode }): ReactElement {
     if (!accountId) return
 
     try {
-      const { name, avatar } = await getEnsProfile(accountId)
+      const name = await getEnsName(accountId)
+      const avatar = getEnsAvatar(name)
 
       if (name) {
         setAccountEns(name)
